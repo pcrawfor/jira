57545627d0e40c3729ffe038c752a81d503578d2
pcrawfor <pcrawfor@gmail.com>
2015-03-12 18:51:29 -0600

    Adding super basic example to doc.go

--- doc.go
+++ doc.go
@@ -3,7 +3,17 @@ Package jira provides a simple wrapper around calls to the JIRA REST API
 
 http://godoc.org/github.com/pcrawfor/jira
 
-examples here.
+// basic example searching for issues with a status of reviewed
+c := jira.NewJiraClient(siteURL, username, password, 100)
+issues, ierr := c.Search("status=reviewed")
+if ierr != nil {
+	fmt.Println("Error: ", ierr)
+	os.Exit(0)
+}
+
+for _, issue := range issues {
+	fmt.Println("Issue:", issue)
+}
 
 */
 package jira
